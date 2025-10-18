f0fae3d8b34b53c3daa8ad6255276086b858f6b6
junheng.liang <junheng.liang@outlook.com>
2025-10-18 14:04:22 +0800

    change ripgrep devDep

--- src/ripgex_util.ts
+++ src/ripgex_util.ts
@@ -1,10 +1,26 @@
-import { rgPath } from "@vscode/ripgrep";
 import { spawnSync } from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
+let ripgrepPath = path.join(
+    vscode.env.appRoot,
+    'node_modules',
+    '@vscode',
+    'ripgrep',
+    'bin',
+    process.platform === 'win32' ? 'rg.exe' : 'rg'
+);
+function getRipGrepPath(): string {
+    if (fs.existsSync(ripgrepPath)) {
+        return ripgrepPath;
+    }
+    ripgrepPath = vscode.workspace.getConfiguration('filter-line').get('ripgrepPath', ripgrepPath);
+    return ripgrepPath;
+}
+
 export function checkRipgrep() {
-    if (fs.existsSync(rgPath)) {
+    if (fs.existsSync(getRipGrepPath())) {
         return true;
     }
     return false;
@@ -16,6 +32,7 @@ export function searchByString(
     pattern: string,
     options: { inverseMatch: boolean, ingoreCase: boolean } = { inverseMatch: false, ingoreCase: false }
 ): Boolean {
+    const rgPath = getRipGrepPath()
     if (!fs.existsSync(rgPath)) {
         vscode.window.showErrorMessage('ripgrep not found!');
         return false;
@@ -47,6 +64,7 @@ export function searchByRegex(
     pattern: string,
     options: { matchSelf: boolean, inverseMatch: boolean, ingoreCase: boolean } = { matchSelf: false, inverseMatch: false, ingoreCase: true }
 ): Boolean {
+    const rgPath = getRipGrepPath()
     if (!fs.existsSync(rgPath)) {
         vscode.window.showErrorMessage('ripgrep not found!')
         return false;
